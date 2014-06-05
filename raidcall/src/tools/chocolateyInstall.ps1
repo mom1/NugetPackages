1558bb7a61d2fc13bde9d9b8060e25485eebb0f2
MOM <mom666@yandex.ru>
2014-06-05 10:17:45 +0400

    RaidCall add

---
+++ raidcall/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+$version = "7.3.4"
+$packageName = 'raidcall' # arbitrary name for the package, used in messages
+$installerType = 'EXE' #only one of these: exe, msi, msu
+$url = "http://update.raidcall.com.ru/download/raidcall_v$version.exe" # download url
+$silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+# main helpers - these have error handling tucked into them already
+# installer, will assert administrative rights
+# if removing $url64, please remove from here
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
