22374c31645ea45205ce67e6b8a08764e0eb4f58
MOM <mom666@yandex.ru>
2014-12-01 02:01:39 +0300

    Uninstall Punto

--- PuntoSwitcher/src/tools/chocolateyUninstall.ps1
+++ PuntoSwitcher/src/tools/chocolateyUninstall.ps1
@@ -1,1 +1,11 @@
-Write-Host "No work to do, all self contained during base uninstall."
+$package = 'PuntoSwitcher'
+
+try {
+  $msiArgs = "/X{57B1BFB9-44BD-4190-954C-37ABB193A557} /qb-! REBOOT=ReallySuppress"
+  Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
