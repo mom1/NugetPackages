5c1b50a426a9a5789a067cd11ac862c5ca1d8747
MOM <mom666@yandex.ru>
2014-12-02 00:43:00 +0300

    Новый пакет dbForge-SQL ru/eng install

---
+++ dbForge-Sql/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'dbforge-sql'
+$installerType = 'EXE'
+$urlru = 'http://www.devart.com/dbforge/sql/studio/dbforgesqlru.exe'
+$urleng = 'http://www.devart.com/dbforge/sql/studio/dbforgesqlfree.exe'
+$silentArgs = '/verysilent'
+$validExitCodes = @(0)
+$locale = (Get-Culture).LCID
+
+if ($locale -eq 1049) {
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$urlru" -validExitCodes $validExitCodes
+} else {
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$urleng" -validExitCodes $validExitCodes
+}
