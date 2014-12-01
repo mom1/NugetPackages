8cfbd611eb6286c7d80d7ddb848347607d832be2
MOM <mom666@yandex.ru>
2014-12-02 01:05:22 +0300

    This package is deprecated use dbforge-sql
    Этот пакет устарел используйте dbforge-sql

--- dbForgeSqlru/src/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'dbForgeSqlru'
-$installerType = 'EXE'
-$url = 'http://www.devart.com/dbforge/sql/studio/dbforgesqlru.exe'
-$silentArgs = '/verysilent'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
