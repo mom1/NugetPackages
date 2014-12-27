a618d534feb301455bf60ec745f6dbec7c78ea6b
MOM <mom666@yandex.ru>
2014-12-28 00:59:35 +0300

    IvideonClient v5.7.0

---
+++ IvideonClient/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'IvideonClient'
+$installerType = 'EXE'
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+$url = "http://downloads-cdn77.iv-cdn.com/bundles/client/IvideonClient_5.7.0_win32_setup.exe"
+if ($is64bit) {
+  $silentArgs = "/S /D=${Env:ProgramFiles(x86)}\Ivideon\IvideonClient"
+} else {
+  $silentArgs = "/S /D=${Env:ProgramFiles}\Ivideon\IvideonClient"
+}
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
