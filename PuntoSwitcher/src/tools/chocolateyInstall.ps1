fdf41a6d2c11e94b41c603fa49e7d792caea708b
MOM <mom666@yandex.ru>
2018-03-30 00:47:15 +0300

    update

--- PuntoSwitcher/src/tools/chocolateyInstall.ps1
+++ PuntoSwitcher/src/tools/chocolateyInstall.ps1
@@ -1,7 +1,10 @@
-﻿$packageName = 'PuntoSwitcher'
-$installerType = 'EXE'
-$url = 'https://download.cdn.yandex.net/punto/PuntoSwitcherSetup_410.exe'
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+﻿$packageArgs = @{
+    packageName = 'PuntoSwitcher'
+    installerType = 'EXE'
+    url = 'https://download.cdn.yandex.net/punto/PuntoSwitcherSetup_441.exe'
+    checksum = 'E51449085AA86932DA8D30907AC8C18A2915774C6D1D99BA4C81757BF464A802'
+    checksumType = 'sha256'
+    silentArgs = '/quiet'
+    validExitCodes = @(0)
+}
+Install-ChocolateyPackage $packageArgs
--- transgui/src/tools/chocolateyInstall.ps1
+++ transgui/src/tools/chocolateyInstall.ps1
@@ -1,6 +1,10 @@
-﻿$version = "5.0.1"
-$packageName = 'transgui'
-$installerType = 'EXE'
-$url = "https://sourceforge.net/projects/transgui/files/$version/transgui-$version-setup.exe/download"
-$silentArgs = '/silent' 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+﻿$version = "5.15.2"
+$packageArgs = @{
+    packageName = 'transgui'
+    installerType = 'EXE'
+    url = "https://github.com/transmission-remote-gui/transgui/releases/download/v$version/transgui-$version-setup.exe"
+    checksum = '6112BBE09FEF0CC153C6F518BDEB661EC1E29EBC222D2AB4C35B5AE484AECF2F'
+    checksumType = 'sha256'
+    silentArgs = '/silent'
+}
+Install-ChocolateyPackage $packageArgs
