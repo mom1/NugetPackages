fbf1c8afe7a84ee59966f564dd84808dbb180fe9
MaxST <mstolpasov@gmail.com>
2019-01-10 21:03:31 +0300

    ivideon

--- ivideon-client/tools/chocolateyInstall.ps1
+++ ivideon-client/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $silentArgs = "/S /D=${Env:ProgramFiles}\Ivideon\IvideonClient"
 $packageArgs = @{
   packageName            = $env:ChocolateyPackageName
   fileType               = 'EXE'
-  url                    = 'http://downloads-cdn77.iv-cdn.com/bundles/client/IvideonClient_6.5.1_win32_setup.exe'
-  url64bit               = 'http://downloads-cdn77.iv-cdn.com/bundles/client/IvideonClient_6.5.1_win32_setup.exe'
-  checksum               = '879CD03BA811BC0233B2E01E5FCFA254BDAC2B5F18BEF8578CFBB25B1B280FC1'
-  checksum64             = '879CD03BA811BC0233B2E01E5FCFA254BDAC2B5F18BEF8578CFBB25B1B280FC1'
+  url                    = 'http://downloads-cdn77.iv-cdn.com/bundles/client/IvideonClient_6.7.1_win32_setup.exe'
+  url64bit               = 'http://downloads-cdn77.iv-cdn.com/bundles/client/IvideonClient_6.7.1_win32_setup.exe'
+  checksum               = '14fbad46f10142660bd15a939b92a20d5375c3b332a954ee08d0f61ddb65d145'
+  checksum64             = '14fbad46f10142660bd15a939b92a20d5375c3b332a954ee08d0f61ddb65d145'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = $silentArgs
--- ivideon-server/tools/chocolateyInstall.ps1
+++ ivideon-server/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $silentArgs = "/S /D=${Env:ProgramFiles}\Ivideon\IvideonServer"
 $packageArgs = @{
   packageName            = $env:ChocolateyPackageName
   fileType               = 'EXE'
-  url                    = 'http://downloads-cdn77.iv-cdn.com/bundles/server/IvideonServer_3.6.1_win32_setup.exe'
-  url64bit               = 'http://downloads-cdn77.iv-cdn.com/bundles/server/IvideonServer_3.6.1_win64_setup.exe'
-  checksum               = 'ad06184523c98c6ad2959d82fc0550bc5216e52a29c9544821948a241846a1bd'
-  checksum64             = 'e26738e70fb6e148f9831f3cfb84935fe91e2b04221465f1dd96a52a0792ccf3'
+  url                    = 'http://downloads-cdn77.iv-cdn.com/bundles/server/IvideonServer_3.7.0_win32_setup.exe'
+  url64bit               = 'http://downloads-cdn77.iv-cdn.com/bundles/server/IvideonServer_3.7.0_win64_setup.exe'
+  checksum               = 'd2561195a1bdc7065e366f205574e1f363c415adf62097f7f28e5d577c35fff6'
+  checksum64             = '0a39c82952ce60a19c8090400b50440c99183c3918e0881b4706ee9dab79a076'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = $silentArgs
